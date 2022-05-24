791eca27bbaf16713411892bf19372342fcb97d5
Martin <martin_lenart@icloud.com>
2022-05-24 12:30:17 +0200

    Update Async

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace Async
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //Random byte[], could be an image
             Random rand = new Random(0);
@@ -14,7 +15,17 @@ namespace Async
             rand.NextBytes(image);
 
             var file = StreamManager.WriteCompressBytes(fname("AnImage.bin"), image);
-            Console.WriteLine($"File written: {file}");
+            Console.WriteLine($"Sync File written: {file}");
+
+            byte[] image2 = StreamManager.ReadCompressBytes(file, image.Length);
+            Console.WriteLine($"Sync File read: {file}, nrOfBytes: {image2.Length}");
+
+
+            var file1 = await StreamManager.WriteCompressBytesAsync(fname("AnAsyncImage.bin"), image);
+            Console.WriteLine($"Async File written: {file1}");
+
+            byte[] image3 = await StreamManager.ReadCompressBytesAsync(fname("AnAsyncImage.bin"), image.Length);
+            Console.WriteLine($"Async File read: {file1}, nrOfBytes: {image3.Length}");
         }
 
         static string fname(string name)
@@ -26,4 +37,14 @@ namespace Async
         }
     }
 }
+
+//Exercises:
+//1.  Make an Async version of StreamManager.WriteCompressBytes(..).
+//2.  Call WriteCompressBytesAsync(..) using await pattern
+//3.  To be able to use await in Main(), Main has to be async, therefore change
+//    "void Main(string[] args)" to "async Task Main(string[] args)"
+//4.  Create a method, byte[] StreamManager.ReadCompressBytes (string path, int NrOfBytes),
+//    and test it with code in Main
+//5.  Create an async version of the method from point 4 above and test it.
+
 
--- Async/StreamManager.cs
+++ Async/StreamManager.cs
@@ -2,11 +2,18 @@
 using System.Text;
 using System.IO;
 using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace Async
 {
 	public static class StreamManager
 	{
+
+        public static Task<string> WriteCompressBytesAsync(string path, byte[] array)
+        {
+            return Task.Run(() => WriteCompressBytes(path, array));
+        }
+
         public static string WriteCompressBytes(string path, byte[] array)
         {
             using (Stream s = File.Create(path))
@@ -15,6 +22,22 @@ namespace Async
             {
                 w.Write(array);
                 return path;
+            }
+        }
+
+        public static Task<byte[]> ReadCompressBytesAsync(string path, int NrOfBytes)
+        {
+            return Task.Run(() => ReadCompressBytes(path, NrOfBytes));
+        }
+
+        public static byte[] ReadCompressBytes(string path, int NrOfBytes)
+        {
+            using (Stream s = File.OpenRead(path))
+            using (Stream ds = new GZipStream(s, CompressionMode.Decompress))
+            using (BinaryReader r = new BinaryReader(ds))
+            {
+                byte[] imageRead = r.ReadBytes(NrOfBytes);
+                return imageRead;
             }
         }
     }
