1f6e8431c73d7a10865909521ddcf7844440b471
Martin <martin_lenart@icloud.com>
2022-05-24 10:15:13 +0200

    Update

---
+++ Async/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random byte[], could be an image
+            Random rand = new Random(0);
+            byte[] image = new byte[10_000_000];
+            rand.NextBytes(image);
+
+            var file = StreamManager.WriteCompressBytes(fname("AnImage.bin"), image);
+            Console.WriteLine($"File written: {file}");
+        }
+
+        static string fname(string name)
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            documentPath = Path.Combine(documentPath, "AOOP2", "Examples");
+            if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+            return Path.Combine(documentPath, name);
+        }
+    }
+}
+
---
+++ Async/StreamManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace Async
+{
+	public static class StreamManager
+	{
+        public static string WriteCompressBytes(string path, byte[] array)
+        {
+            using (Stream s = File.Create(path))
+            using (Stream ds = new GZipStream(s, CompressionMode.Compress))
+            using (BinaryWriter w = new BinaryWriter(ds))
+            {
+                w.Write(array);
+                return path;
+            }
+        }
+    }
+}
+
