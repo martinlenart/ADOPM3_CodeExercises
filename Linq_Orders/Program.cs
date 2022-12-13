93aeb4e9948b7522e25724b72bd1bca0e487bb47
Martin Lenart <martin_lenart@icloud.com>
2022-12-13 14:43:25 +0100

    Revert "Exercises completed"
    This reverts commit 01ca02d311bb726242bc399092c1fc4a4cf65e73.

--- Linq_Orders/Program.cs
+++ Linq_Orders/Program.cs
@@ -14,60 +14,8 @@ namespace Linq_Orders
                 OrderList.Add(Order.Factory.CreateWithRandomData());
             }
 
-            Console.WriteLine($"OrderCount: {OrderList.Count()}\nOrderValue: {OrderList.Sum(o => o.Total):C2}");
-
-            Console.WriteLine("\nLargest 5 orders:");
-            OrderList.OrderByDescending(o => o.Total).Take(5).ToList().ForEach(o => Console.WriteLine(o));
-            Console.WriteLine($"\nOrder count with total < 1000: {OrderList.Where(o => o.Total<1000).Count()}");
-            Console.WriteLine($"\nOrder count with total < 1000: {OrderList.Count(o => o.Total < 1000)}");
-
-            Console.WriteLine($"Freightsum of orders with total < 1000: {OrderList.Where(o => o.Total < 1000).Sum(o => o.Freight):C2}");
-
-            Console.WriteLine("\nCountries with orders");
-            var countries = OrderList.Select(o => o.Country);
-            foreach (var item in countries.Distinct())
-            {
-                Console.WriteLine(item);
-            }
-
-            var lateDelCount = OrderList.Count(o =>
-            {
-                if (o.DeliveryDate == null) return false;
-
-                var days = (o.DeliveryDate - o.OrderDate).Value.Days;
-                return days > 15;
-            });
-            Console.WriteLine($"\nOrder count with delivery time > 15 days: {lateDelCount}");
-
-            var finnishOrders = OrderList.Where(o => o.Country == "Finland");
-            Console.WriteLine($"\nOrder count Finland: {finnishOrders.Count()}");
-            Console.WriteLine($"Ordersum Finland: {finnishOrders.Sum(o => o.Total):C2}");
-
-            Console.WriteLine("\nOrders and Order value per country");
-            var groupedList = OrderList.GroupBy(o => o.Country, order => order);
-            foreach (var group in groupedList)
-            {
-                Console.WriteLine($"Orders {group.Key}:");
-                Console.WriteLine($"   Order count: {group.Count()}\n   Order value: {group.Sum(o => o.Total):C2}");
-            }
-
-            Console.WriteLine("\n5 largest Orders per country");
-            foreach (var group in groupedList)
-            {
-                Console.WriteLine($"Orders {group.Key}:");
-                var largestOrders = group.OrderByDescending(o => o.Total).Take(5);
-                //largestOrders.ToList().ForEach(o => Console.WriteLine(o));
-
-                foreach (var item in largestOrders)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-   
-            var avgDays = OrderList.Where(o => o.DeliveryDate.HasValue).Average(o => (o.DeliveryDate.Value - o.OrderDate).Days);
-            Console.WriteLine($"\nAverage Delivery time: {avgDays}");
-
-        }
+            Console.WriteLine($"OrderCount: {OrderList.Count()}");
+         }
     }
 }
 //Exercises:
