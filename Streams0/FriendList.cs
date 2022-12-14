81976e4ab9ca94e2f9d7959e0dd86741416defa7
Martin Lenart <martin_lenart@icloud.com>
2022-12-14 12:57:58 +0100

    update

---
+++ Streams0/FriendList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Streams0
+{
+    public class FriendList
+    {
+        public  List<Friend> myFriends = new List<Friend>();
+        public Friend this[int idx]=> myFriends[idx];
+
+        public override string ToString()
+        {
+            string sRet = "";
+            foreach (var item in myFriends)
+            {
+                sRet += item.ToString() + "\n";
+            }
+            return sRet;
+        }
+
+        public static class Factory
+        {
+            public static FriendList CreateRandom(int NrOfItems)
+            {
+
+                var myList = new FriendList();
+                for (int i = 0; i < NrOfItems; i++)
+                {
+                    var afriend = Friend.Factory.CreateRandom();
+                    myList.myFriends.Add(afriend);
+                }
+                return myList;
+            }
+        }
+
+        /// <summary>
+        /// Write the text representation of the instance of FriendList to a text file
+        /// </summary>
+        /// <param name="txtFileName">The text file to write to</param>
+        /// <returns></returns>
+        public string WriteToDisk(string txtFileName)
+        {
+            //Your Code
+ 
+            return fname(txtFileName);
+        }
+
+        /// <summary>
+        /// Write a ziped compressed of file the text representation of the instance of FriendList
+        /// </summary>
+        /// <param name="zipFileName">Zip file to write the compressed content to</param>
+        /// <returns></returns>
+        public string WriteToDiskCompressed(string zipFileName)
+        {
+            //Your Code
+
+            return fname(zipFileName);
+        }
+
+        /// <summary>
+        /// Uncompresses the zipFile and writes the uncompressed content to a texfile
+        /// </summary>
+        /// <param name="zipFileName">Zip file to read and uncompress</param>
+        /// <param name="txtFileName">Text file to write the uncompressed content</param>
+        /// <returns></returns>
+        public string UncompressToDisk(string zipFileName, string txtFileName)
+        {
+            //read from stream
+            //Your Code
+
+            //write to stream
+            //Your Code
+
+            return fname(txtFileName);
+        }
+
+        static string fname(string name)
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            documentPath = Path.Combine(documentPath, "ADOP", "Streams");
+            if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+            return Path.Combine(documentPath, name);
+        }
+    }
+}
---
+++ Streams0/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Streams0 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var friendsToDisk = FriendList.Factory.CreateRandom(1_000);
+            
+            var s = friendsToDisk.WriteToDisk("Friends.txt");
+            Console.WriteLine(s);
+
+            s = friendsToDisk.WriteToDiskCompressed("Friends.zip");
+            Console.WriteLine(s);
+
+            s = friendsToDisk.UncompressToDisk("Friends.zip", "Friends2.txt");
+            Console.WriteLine(s);
+
+        }
+    }
+}
+
+//Exercises:
+//1. Implement friendsToDisk.WriteToDisk, friendsToDisk.WriteToDiskCompressed and friendsToDisk.UncompressToDisk
+//   Write to disk and study the files created. The content of the txt files and compare the size to the compressed file
