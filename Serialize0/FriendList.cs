783abaaf54f054348a6a4a6d6a7567b3d0b70694
Martin Lenart <martin_lenart@icloud.com>
2022-12-14 14:29:23 +0100

    update

---
+++ Serialize0/FriendList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+namespace Serialization0
+{
+    public class FriendList
+    {
+        //Note myFriends { get; set; } - it needs to be a public property for Json serialization
+        public List<Friend> myFriends { get; set; } = new List<Friend>();
+        public Friend this[int idx]=> myFriends[idx];
+
+        public override string ToString()
+        {
+            string sRet = "";
+            foreach (var item in myFriends)
+            {
+                sRet += item.ToString() + "\n";
+            }
+            return sRet;
+        }
+
+        public static class Factory
+        {
+            public static FriendList CreateRandom(int NrOfItems)
+            {
+
+                var myList = new FriendList();
+                for (int i = 0; i < NrOfItems; i++)
+                {
+                    var afriend = Friend.Factory.CreateRandom();
+                    myList.myFriends.Add(afriend);
+                }
+                return myList;
+            }
+        }
+
+        public void SerializeXml(string xmlFileName)
+        {
+            //Your Code
+        }
+        public static FriendList DeSerializeXml(string xmlFileName)
+        {
+            //Your Code
+            return null; //your code
+        }
+        public void SerializeJson(string jsonFileName)
+        {
+            //Your Code
+        }
+        public static FriendList DeSerializeJson(string jsonFileName)
+        {
+            //Your Code
+            return null; //your code
+        }
+
+        static string fname(string name)
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            documentPath = Path.Combine(documentPath, "ADOP", "Serialization");
+            if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+            return Path.Combine(documentPath, name);
+        }
+    }
+}
---
+++ Serialize0/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Serialization0 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var friendsToDisk = FriendList.Factory.CreateRandom(1_000);
+            Console.WriteLine(friendsToDisk.myFriends.Count);
+
+            friendsToDisk.SerializeXml("Friends.xml");
+            var xmlFriends = FriendList.DeSerializeXml("Friends.xml");
+
+            Console.WriteLine(xmlFriends?.myFriends.Count);
+
+            friendsToDisk.SerializeJson("Friends.json");
+            var jsonFriends = FriendList.DeSerializeJson("Friends.json");
+
+            Console.WriteLine(jsonFriends?.myFriends.Count);
+        }
+    }
+}
+//Exercise:
+//1. Implement SerializeXml(), DeSerializeXml(), SerializeJson(), and DeSerializeJson()
