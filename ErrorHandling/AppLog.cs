9c0fc1e9e59fa702effced4e1e613c7577b584ff
Martin Lenart <martin_lenart@icloud.com>
2022-12-14 09:32:05 +0100

    update

---
+++ ErrorHandling/AppLog.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    public class AppLogItem
+    {        
+        public DateTime Time {get; set;}
+        public string Type { get; set; }
+        public string[] Info { get; set; }
+        public override string ToString()
+        {
+            var sReturn = $"{Type} at {Time}:\n";
+            foreach (var item in Info)
+            {
+                sReturn += $"{item}\n";
+            }
+            return sReturn;
+        }
+    }
+
+    //Example of a logger using Singleton design pattern
+    public sealed class AppLog
+    {
+        const string LoggerFile = "CodeExercise_Logger.text";
+        private static AppLog _instance = null;
+
+        private static Stack<AppLogItem> _logStack = null;
+
+        //This is key for Singleton
+        private AppLog()
+        {
+            _logStack = new Stack<AppLogItem>();
+        }
+
+        //This is the key for Singelton
+        public static AppLog Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppLog();
+                }
+                return _instance;
+            }
+        }
+
+        public void WriteToDisk()
+        {
+            using (Stream s = File.Create(fname(LoggerFile)))
+            using (TextWriter w = new StreamWriter(s))
+            {
+                foreach(var item in this.ToList())
+                {
+                    w.WriteLine(item);
+                }
+             }          
+        }
+
+        public void LogInformation(params string[] info)
+        {
+            _logStack.Push(new AppLogItem { Time = DateTime.Now, Type = "Information", Info = info });
+        }
+
+        public void LogException(Exception ex)
+        {
+            var item = new AppLogItem();
+            item.Time = DateTime.Now;
+            item.Type = "Exception";
+            item.Info = new string[3];
+            item.Info[0] = ex?.GetType().Name;
+            item.Info[1] = ex?.Message;
+            item.Info[2] = ex?.InnerException?.Message;
+            _logStack.Push(item);
+        }
+
+        public List<AppLogItem> ToList()
+        {
+            return _logStack.ToList<AppLogItem>();
+        }
+
+        string fname(string name)
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            documentPath = Path.Combine(documentPath, "ADOP", "Examples");
+            if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+            return Path.Combine(documentPath, name);
+        } 
+    }
+}
+
---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProcessUserInput();
+            }
+            catch (Exception ex)
+            {
+                AppLog.Instance.LogException(ex);
+                Console.WriteLine("Pls contact our service center, you have a virus in your computer");
+            }
+            finally
+            {
+                AppLog.Instance.WriteToDisk();
+            }
+        }
+        private static void ProcessUserInput()
+        {
+            bool quit = false;
+            Console.WriteLine("Don't Press Button 5 or 7!");
+            do
+            {
+                Console.WriteLine("Which button do you want to press?");
+                string sButtonToPress = Console.ReadLine();
+
+                if (sButtonToPress == "q")
+                {
+                    quit = true;
+                }
+                else
+                {
+                    int buttonToPress;
+                    if (int.TryParse(sButtonToPress, out buttonToPress))
+                    {
+                        try
+                        {
+                            PressTheButton(buttonToPress);
+                            Console.WriteLine("Indeed the button was pressed successfully");
+                        }
+                        catch (InsufficientMemoryException ex)
+                        {
+                            AppLog.Instance.LogException(ex);
+                            Console.WriteLine($"{ex.Message} - Why cant you listen!!");
+                            throw;
+                        }
+                        catch (Exception ex)
+                        {
+                            AppLog.Instance.LogException(ex);
+                            Console.WriteLine($"{ex.Message} - But it is alright my friend!");
+                        }
+                        finally
+                        {
+                            Console.WriteLine("Code here will always be executed!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input, try again");
+                    }
+                }
+            } while (!quit);
+        }
+        static void PressTheButton(int buttonNr)
+        {
+            if (buttonNr == 5)
+                throw new Exception("KaBoom!!");
+
+            if (buttonNr == 7)
+                throw new InsufficientMemoryException("You hopless guy!");
+
+            Console.WriteLine($"You pressed button {buttonNr}");
+        }
+
+    }
+}
+
+//Exercise:
+//1. Change the messages thrown in PressTheButton() to your own ExceptionMessage, ExplosionException inheriting from Exception.
+//   Create a property in ExplosionException that of the button pressed and severity of the error (managable and fatal)
+//2. Modify the code in ProcessUserInput() to catch ExplosionException and depending on severity gives an user message(manageable) or rethrow (fatal)
