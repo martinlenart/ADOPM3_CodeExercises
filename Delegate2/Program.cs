1ef78ab315907f917bcfb394edc2e3b7435cc6da
Martin Lenart <martin_lenart@icloud.com>
2022-12-19 09:54:45 +0100

    update

--- Delegate2/Program.cs
+++ Delegate2/Program.cs
@@ -23,17 +23,34 @@ namespace Delegate2 // Note: actual namespace depends on the project name.
             Console.WriteLine("\nHello to Scandinavia");
             friends.SayHello(HelloScandinavia);
 
+            Console.WriteLine("\nAlternative Hello to All");
+            Action<Friend> AllHello = HelloFinland;
+            AllHello += HelloGavle;
+            AllHello += HelloScandinavia;
+            friends.SayHello(AllHello);
 
         }
 
         public static void HelloFinland(Friend friend)
         {
+            if (friend.Address.Country == "Finland")
+            {
+                Console.WriteLine($"Hello {friend.FirstName}, {friend.Address.Country} from Finland");
+            }
         }
         public static void HelloGavle(Friend friend)
         {
+            if (friend.Address.City == "Gavle")
+            {
+                Console.WriteLine($"Hello {friend.FirstName}, {friend.Address.City} from Gavle");
+            }
         }
         public static void HelloScandinavia(Friend friend)
         {
+            if (friend.Address.Country != "Finland")
+            {
+                Console.WriteLine($"Hello {friend.FirstName}, {friend.Address.Country} from Scandinavia");
+            }
         }
 
 
---
+++ Task1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Message = "Martins hello";
+                var t1 = Task.Run(() =>
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine($"{Message}{i} from Task1");
+                        Task.Delay(200);
+                        if (i == 5)
+                        {
+                            //throw new Exception("Task1 has faulted");
+                        }
+                    }
+
+                    return "Task1 has completed";
+                });
+                Console.WriteLine(t1.Result);
+
+                var t2 = Task.Run(() =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                        Console.WriteLine($"{Message}{i} from Task2");
+                        Task.Delay(1000);
+                        if (i == 5)
+                        {
+                            //throw new Exception("Task2 has faulted");
+                        }
+
+                    }
+                    return "Task2 has completed";
+                });
+                Console.WriteLine(t2.Result);
+
+                var t3 = Task.Run(() =>
+                {
+                    for (int i = 0; i < 15; i++)
+                    {
+                        Console.WriteLine($"{Message}{i} from Task3");
+                        Task.Delay(500);
+                        if (i == 5)
+                        {
+                            //throw new Exception("Task3 has faulted");
+                        }
+                    }
+                    return "Task3 completed";
+                });
+                Console.WriteLine(t3.Result);
+            }
+            catch (Exception ex)
+            {
+                //Your code
+            }
+            finally
+            {
+                Console.WriteLine("Main terminated");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+//Exercise:
+//1. Write code in the exception handler to check what task faulted and write an error message containing the message from the faulted task
+//2. Experiement by letting one or several of the tasks simulate an error by throwing an Exception. See that you get an error message
+//3. What happens if two or more tasks fault?
