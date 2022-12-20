922a700d746b21bfeae9da827d5fb0990a997704
Martin Lenart <martin_lenart@icloud.com>
2022-12-20 11:11:46 +0100

    update

---
+++ Task3/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Task3
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = null, s2 = null, s3 = null;
+            Task<string> t1 = null, t2 = null, t3 = null;
+            try
+            {
+                Console.WriteLine("Syncron calls");
+                s1 = SayHello("Good Morning", 10, 1000, false);
+                s2 = SayHello("Good Afternoon", 5, 2000, false);
+                s3 = SayHello("Good Evening", 15, 500, false);
+
+                Console.WriteLine(s1);
+                Console.WriteLine(s2);
+                Console.WriteLine(s3);
+
+                Console.WriteLine("\n\nAsyncron calls");
+                //Ex3 - make the calls to SayHelloAsync
+                t1 = SayHelloAsync("Good Morning", 10, 1000, false);
+                Console.WriteLine(t1.Result);
+
+                t2 = SayHelloAsync("Good Afternoon", 5, 2000, false);
+                Console.WriteLine(t2.Result);
+
+                t3 = SayHelloAsync("Good Evening", 15, 500, false);
+                Console.WriteLine(t3.Result);
+            }
+            catch (Exception ex)
+            {
+                //Your code
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Main terminated");
+                Console.ReadLine();
+            }
+        }
+
+        //Ex2 - complete below async declaration of SayHello 
+        //static public Task<string> SayHelloAsync(string message, int iterations, int msDelay, bool causeError = false) => ....
+
+        static public Task<string> SayHelloAsync(string message, int iterations, int msDelay, bool causeError = false) =>
+            Task.Run(() => SayHello(message, iterations, msDelay, causeError));
+
+        static public string SayHello(string message, int iterations, int msDelay, bool causeError = false)
+        {
+            var rnd = new Random();
+            int errorIteration = rnd.Next(0, iterations);
+            for (int i = 0; i< iterations; i++)
+            {
+                Console.WriteLine($"{i,4}:{message}");
+                Task.Delay(msDelay);
+                
+                if (causeError && (i == errorIteration))
+                {
+                    throw new Exception($"Error saying: {message}");
+                }
+            }
+            return $"All good saying: {message}";
+        }
+    }
+}
+
+//Exercise:
+//1. Modify above code to follow the async/await pattern
+//2. Experiment by setting causeError = true for the various async calls
+//   - understand the error handling
+
+
+
+
+
+
+
