5698712055aa7cbd59e8f22cc7e21e3d02f6285e
Martin <martin_lenart@icloud.com>
2023-12-12 16:01:34 +0100

    update

---
+++ Serialize1/csWine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Helpers;
+
+namespace _05_Wines_Interfaces
+{
+    public enum enGrapeType { Reissling, Tempranillo, Chardonay, Shiraz, CabernetSavignoin, Syrah }
+    public enum enWineType { Red, White, Rose }
+    public enum enCountry { Germany, France, Spain }
+
+    public class csWine
+	{
+        public string Name { get; set; }
+
+        public enCountry Country { get; set; }
+        public enWineType WineType { get; set; }
+        public enGrapeType GrapeType { get; set; }
+
+        public decimal Price { get; set; }
+
+        public override string ToString()
+            => $"Wine {Name} from {Country} is {WineType} and made from grapes {GrapeType}."
+            + $" The price is {Price:N2} Sek. ({this.GetType().Name})";
+
+
+        public csWine Seed (csSeedGenerator rnd)
+        {
+            Name = rnd.FromString("Chattaux de bueff, Chattaux de paraply, PutiPuti, NamNam");
+
+            GrapeType = rnd.FromEnum<enGrapeType>();
+            WineType = rnd.FromEnum<enWineType>();
+            Country = rnd.FromEnum<enCountry>();
+            Price = rnd.Next(50, 150);
+            return this;
+        }
+
+        public csWine(csWine original)
+        {
+            this.Name = original.Name;
+            this.GrapeType = original.GrapeType;
+            this.Country = original.Country;
+            this.WineType = original.WineType;
+            this.Price = original.Price;
+        }
+        public csWine()
+        {
+
+        }
+	}
+}
+
---
+++ Serialize1/csWineCellar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace _05_Wines_Interfaces
+{
+    public class WineCellar
+    {
+        public string Name { get; set; }
+        public List<csWine> Wines { get; } = new List<csWine>();
+
+        public void Add(csWine wine) => Wines.Add(wine);
+        public int Count => Wines.Count;
+
+        public decimal Value
+        {
+            get
+            {
+                decimal _sum = 0M;
+                foreach (var wine in Wines)
+                {
+                    _sum += wine.Price;
+                }
+                return _sum;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sRet = "";
+            foreach (var wine in Wines)
+            {
+                sRet += $"{wine}\n";
+            }
+            return sRet;
+        }
+
+        public WineCellar() { }
+        public WineCellar(string name)
+        {
+            Name = name;
+        }
+
+        public (csWine hicost, csWine locost) WineHiLoCost()
+        {
+            if (Wines.Count == 0) return (null,null);
+
+            decimal _hiPrice = decimal.MinValue;
+            csWine _hiWine = null;
+            decimal _loPrice = decimal.MaxValue;
+            csWine _loWine = null;
+            foreach (var wine in Wines)
+            {
+                if (wine.Price > _hiPrice)
+                {
+                    _hiWine = wine;
+                    _hiPrice = wine.Price;
+                }
+                if (wine.Price < _loPrice)
+                {
+                    _loWine = wine;
+                    _loPrice = wine.Price;
+                }
+            }
+            return (_hiWine, _loWine);
+        }
+    }
+}
+
