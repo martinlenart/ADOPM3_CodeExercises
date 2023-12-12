40c4aeeb39036b06ebce5664a65e1f53698376d2
Martin <martin_lenart@icloud.com>
2023-12-12 16:35:34 +0100

    update

--- Serialize0/FriendList.cs
+++ Serialize0/FriendList.cs
@@ -42,43 +42,23 @@ namespace Serialization0
         public void SerializeXml(string xmlFileName)
         {
             //Your Code
-            var xs = new XmlSerializer(typeof(FriendList));
-            using (Stream s = File.Create(fname(xmlFileName)))
-            {
-                xs.Serialize(s, this);
-            }
+
         }
         public static FriendList DeSerializeXml(string xmlFileName)
         {
             //Your Code
-            var xs = new XmlSerializer(typeof(FriendList));
-            FriendList flist;
-            using (Stream s = File.OpenRead(fname(xmlFileName)))
-            {
-                flist = (FriendList)xs.Deserialize(s);
-                return flist;
-            }
+            return null;
         }
         public void SerializeJson(string jsonFileName)
         {
             //Your Code
-            string sJson = JsonSerializer.Serialize<FriendList>(this, new JsonSerializerOptions() { WriteIndented = true });
-            
-            using (Stream s = File.Create(fname(jsonFileName)))
-            using (TextWriter writer = new StreamWriter(s))
-                writer.Write(sJson);
+
 
         }
         public static FriendList DeSerializeJson(string jsonFileName)
         {
             //Your Code
-            using (Stream s = File.OpenRead(fname(jsonFileName)))
-            using (TextReader reader = new StreamReader(s))
-            {
-                string sTmp = reader.ReadToEnd();
-                var flist = JsonSerializer.Deserialize<FriendList>(sTmp);
-                return flist;
-            }
+            return null;
         }
 
         static string fname(string name)
