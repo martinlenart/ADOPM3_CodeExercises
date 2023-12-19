2a80b6b581018ac3b2e96fd39487b49355f0b9cc
Martin <martin_lenart@icloud.com>
2023-12-19 13:12:50 +0100

    update

--- Event3/Program.cs
+++ Event3/Program.cs
@@ -29,7 +29,7 @@ class Program
 }
 /* Exercises 
    1. Go through the code and understand the execution flow
-   2. Write a class ServiceLogger that stores every Factory Alarm which is an error
+   2. Write a class ServiceLogger that stores every Factory Alarm Static Code which is an error
    3. Before Factory Check complete, the list of of factory alarams should be written out
 */
 
