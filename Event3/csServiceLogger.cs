30ef62884a244e12d92ce5434f7177efec8e7d28
Martin <martin_lenart@icloud.com>
2023-12-19 13:23:06 +0100

    service class in progress

---
+++ Event3/csServiceLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ADOPM3_02_18a
+{
+	public class csServiceLogger
+	{
+		public List<int> CodeLog = new List<int>();
+		public csServiceLogger(csFactoryMontor fm)
+		{
+			fm.AlarmStatus += LogACode;
+		}
+
+		void LogACode(int StatusCode)
+		{
+			CodeLog.Add(StatusCode);
+		}
+
+	}
+}
+
