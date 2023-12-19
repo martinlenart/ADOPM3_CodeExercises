1cb73b899f7fe10c60d7d88d68b0cc3e01e457c2
Martin <martin_lenart@icloud.com>
2023-12-19 13:37:16 +0100

    doen deal

--- Event3/csServiceLogger.cs
+++ Event3/csServiceLogger.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 namespace ADOPM3_02_18a
 {
+	public class csLogEntry
+	{
+		public int StatusCode { get; set; }
+		public string Message { get; set; }
+		public override string ToString() => $"{StatusCode}, {Message}";
+    }
 	public class csServiceLogger
 	{
-		public List<(int, string)> CodeLog = new List<(int, string)>();
+		public List<csLogEntry> CodeLog = new List<csLogEntry>();
 		public csServiceLogger(csFactoryMontor fm)
 		{
 			fm.AlarmStatus += LogACode;
@@ -11,14 +17,19 @@ namespace ADOPM3_02_18a
 
 		void LogACode(int StatusCode)
 		{
-			CodeLog.Add((StatusCode, (StatusCode) switch
-			{
-				0 => $"All Good",
-				1 => $"Easy Level",
-				2 => $"Moderate Level",
-				3 => $"Critical Level",
-				_ => $"Unknown Level"
-			}));
+			CodeLog.Add(new csLogEntry()
+				{
+					StatusCode = StatusCode,
+					Message = (StatusCode) switch
+					{
+						0 => $"All Good",
+						1 => $"Easy Level",
+						2 => $"Moderate Level",
+						3 => $"Critical Level",
+						_ => $"Unknown Level"
+					}
+				}
+			);
 		}
 
 	}
