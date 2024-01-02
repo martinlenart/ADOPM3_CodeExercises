7b45897504ef969f85ac5e061986ba88b92ad6d4
Martin <martin_lenart@icloud.com>
2024-01-02 15:45:27 +0100

    updated ThreadSafeData

--- ThreadSafeData/Program.cs
+++ ThreadSafeData/Program.cs
@@ -6,15 +6,56 @@ namespace ThreadSafeData
 {
     class Program
     {
-        //Thread Safe Datastructure
         public class Vehicle
         {
-            string RegistrationNumber;
-            string Owner;
+            public string RegistrationNumber { get; set; }
+            public string Owner { get; set; }
         }
+
+        //Thread Safe Datastructure
+        public class VehicleStorage
+        {
+            object _locker = new object();
+            List<Vehicle> _vehicles = new List<Vehicle>();
+
+            public void SetData (string regNr, string owner)
+            {
+                lock (_locker)
+                {
+                    var v = new Vehicle() { RegistrationNumber = regNr, Owner = owner };
+                    _vehicles.Add(v);
+                }
+            }
+
+            public Vehicle GetData (int idx)
+            {
+                lock(_locker)
+                {
+                    return _vehicles[idx];
+                }
+            }
+
+            public bool CheckConsistency()
+            {
+                lock (_locker)
+                {
+                    foreach (var v in _vehicles)
+                    {
+                        //Verify data consistency - give error if not consistent
+                        if ((v.RegistrationNumber, v.Owner) != ("ABC 123", "Kalle Anka") &&
+                            (v.RegistrationNumber, v.Owner) != ("HKL 556", "Musse Pigg"))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            var myCar = new Vehicle();
+            var myCar = new VehicleStorage();
             var rnd = new Random();
 
             var t1 = Task.Run(() =>
@@ -23,14 +64,19 @@ namespace ThreadSafeData
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "ABC 123", "Kalle Anka"
+                    myCar.SetData("ABC 123", "Kalle Anka");
 
                     //introduce some system delay
-                    //Task.Delay(rnd.Next(1, 5)).Wait();
+                    Task.Delay(rnd.Next(1, 5)).Wait();
 
                     //Read Data from Vehicle
+                    var t = myCar.GetData(i);
 
-                    //Verify data consistency - give error if not consistent                   
-                 }
+                    if (!myCar.CheckConsistency())
+                    {
+                        Console.WriteLine("Data inconsistent!");
+                    }
+                }
                 Console.WriteLine("t1 Finished");
             });
 
@@ -40,13 +86,19 @@ namespace ThreadSafeData
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "HKL 556", "Musse Pigg"
+                    myCar.SetData("HKL 556", "Musse Pigg");
 
                     //introduce some system delay
-                    //Task.Delay(rnd.Next(1, 5)).Wait();
+                    Task.Delay(rnd.Next(1, 5)).Wait();
 
                     //Read Data from Vehicle
+                    var t = myCar.GetData(i);
 
-                    //Verify data consistency - give error if not consistent                   
+                    //Verify data consistency - give error if not consistent
+                    if (!myCar.CheckConsistency())
+                    {
+                        Console.WriteLine("Data inconsistent!");
+                    }
                 }
                 Console.WriteLine("t2 Finished");
             });
@@ -54,6 +106,8 @@ namespace ThreadSafeData
             Task.WaitAll(t1, t2);
             Console.WriteLine("All Finished");
         }
+
+
     }
 }
 /*  Exercise
